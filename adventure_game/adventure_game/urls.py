9a4b4637fc408498bc24abb0651ac72286d233da
Dong Liang <dliang04@citymail.cuny.edu>
2015-11-09 23:07:04 -0500

    map is on template

--- adventure_game/adventure_game/urls.py
+++ adventure_game/adventure_game/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('coreapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^map/$', include('map.urls')),
 ]
