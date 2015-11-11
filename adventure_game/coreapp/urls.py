c9e34c09e8a95df81aa8fc58cc795c37a880980f
Linfinity <wlin15@citymail.cuny.edu>
2015-11-10 20:34:39 -0500

    login done

--- adventure_game/coreapp/urls.py
+++ adventure_game/coreapp/urls.py
@@ -4,4 +4,7 @@ from coreapp import views
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^auth/$', views.auth_view, name='auth'),
+    url(r'^invalid/$', views.invalid_login),
+    url(r'^logout/$', views.logout),
 )
--- adventure_game/coreapp/views.py
+++ adventure_game/coreapp/views.py
@@ -1,23 +1,35 @@
 from django.shortcuts import get_object_or_404, render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth import authenticate, login
+from django.contrib import auth
 from django.core.context_processors import csrf #user security
 
 
 def home(request):
     return render(request, 'coreapp/home.html')
 
-
 def login(request):
-	c = {}
-	c.update(csrf(request))
-	return render_to_response('auth/login.html', c)
+	context = {}
+	context.update(csrf(request))
+	return render_to_response('auth/login.html', context)
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/invalid')
+    else:
+        return HttpResponseRedirect('/invalid')
+
+def invalid_login(request):
+	return render_to_response('auth/invalid_login.html')
 
 
-def my_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    # if user is not None:
-    #
-    # else:
+def logout(request):
+    auth.logout(request)
+    # context = {'page': 'home'}
+    return HttpResponseRedirect('/')
