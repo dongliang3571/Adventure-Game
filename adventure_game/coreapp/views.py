9bf20d7d468c251883f828668a1a2a5dad8c2fc4
Linfinity <wlin15@citymail.cuny.edu>
2015-11-11 17:55:49 -0500

    add registration submission

--- adventure_game/coreapp/views.py
+++ adventure_game/coreapp/views.py
@@ -28,8 +28,22 @@ def auth_view(request):
 def invalid_login(request):
 	return render_to_response('auth/invalid_login.html')
 
-
 def logout(request):
     auth.logout(request)
     # context = {'page': 'home'}
     return HttpResponseRedirect('/')
+
+def registration_submission(request):
+    username = request.POST.get('username', '')
+    firstname = request.POST.get('firstname', '')
+    lastname = request.POST.get('lastname', '')
+    email = request.POST.get('email', '')
+    password = request.POST.get('password', '')
+    if len(User.objects.filter(username=username)) != 0: #pylint: disable=E1101
+        return registration(request, "Try again, the username %s %s." %(username, "is already taken"))
+    if len(User.objects.filter(email=email)) != 0: #pylint: disable=E1101
+        return registration(request, "Try again, %s %s." %("there is already an account with email", email))
+    user = User.objects.create_user(username=username, email=email, password=password, first_name=firstname, last_name=lastname) #pylint: disable=E1101
+    user = auth.authenticate(username=username, password=password)
+    auth.login(request, user)
+    return HttpResponseRedirect('/')#'/registration-success')
