af913787d69aa358b62e450f3e42221c3b6ce198
Meghann B Bledsoe <mblesdo00@citymail.cuny.edu>
2015-11-12 10:24:31 -0500

    registration test

--- adventure_game/coreapp/tests.py
+++ adventure_game/coreapp/tests.py
@@ -3,6 +3,12 @@ from django.test import Client
 
 #login test
 def login_test(self):
-    c = Client()
-    response = c.post('/login/',{'username': 'jennifer', 'password': 'secret'})
+    user = Client()
+    response = user.post('/login/',{'username': 'jennifer', 'password': 'secret'})
     self.assertEqual(response.status_code,200)
+
+#registration test
+def regi_test(self):
+    new_user = Client()
+    info = new_user.post('/registration/',{'username': 'sam123', 'email': 'abc@gmail.com', 'password': 'abc123'})
+    self.assertEqual(.status_code,200)
