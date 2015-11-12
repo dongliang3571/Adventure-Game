359c8ca53ba81a35a7c6253efda2c932fc453dd8
Meghann B Bledsoe <mblesdo00@citymail.cuny.edu>
2015-11-12 10:29:36 -0500

    cleaned up test structure

--- adventure_game/coreapp/tests.py
+++ adventure_game/coreapp/tests.py
@@ -1,14 +1,17 @@
+import unittest
 from django.test import TestCase
 from django.test import Client
 
-#login test
-def login_test(self):
-    user = Client()
-    response = user.post('/login/',{'username': 'jennifer', 'password': 'secret'})
-    self.assertEqual(response.status_code,200)
+class AccountTests(unittest.TestCase):
 
-#registration test
-def regi_test(self):
-    new_user = Client()
-    info = new_user.post('/registration/',{'username': 'sam123', 'email': 'abc@gmail.com', 'password': 'abc123'})
-    self.assertEqual(info.status_code,200)
+    def setUp(self):
+        self.user = Client()
+    #registration test
+    def regi_test(self):
+        info = self.user.post('/registration/',{'username': 'sam123', 'email': 'abc@gmail.com', 'password': 'abc123'})
+        self.assertEqual(info.status_code,200)
+
+    #login test
+    def login_test(self):
+        response = self.user.post('/login/',{'username': 'jennifer', 'password': 'secret'})
+        self.assertEqual(response.status_code,200)
