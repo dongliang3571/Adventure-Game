1aa73535c3308ca899e64d0d7865e73e52af2304
Meghann B Bledsoe <mblesdo00@citymail.cuny.edu>
2015-11-12 10:13:12 -0500

    login test

--- adventure_game/coreapp/tests.py
+++ adventure_game/coreapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+#login test
+def login_test(self):
+    c = Client()
+    response = c.post('/login/',{'username': 'jennifer', 'password': 'secret'})
+    self.assertEqual(response.status_code,200)
