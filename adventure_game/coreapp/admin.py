da1b6f7cefc006b3130ad9c635c560cd7e0cdbd2
Linfinity <wlin15@citymail.cuny.edu>
2015-11-17 17:37:25 -0500

    fix some typo

--- adventure_game/coreapp/admin.py
+++ adventure_game/coreapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile, character
+from .models import UserProfile, PIN
 
 # Register your models here.
-admin.site.register(UserProfile, character)
+admin.site.register(UserProfile)
+admin.site.register(PIN)
