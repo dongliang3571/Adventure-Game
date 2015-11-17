d58382a52fe02c69e11239124eb28b71056976c2
Linfinity <wlin15@citymail.cuny.edu>
2015-11-17 17:12:40 -0500

    add model pin

--- adventure_game/coreapp/models.py
+++ adventure_game/coreapp/models.py
@@ -5,3 +5,7 @@ class UserProfile(models.Model):
     # user = models.ForeignKey(User, unique=True)
     user = models.OneToOneField(User)
     character = models.CharField(max_length=200)
+
+class PIN(models.Model):
+    character = models.ForeignKey(character)
+    character_pin = models.CharField(max_length=4, min_length=4)
