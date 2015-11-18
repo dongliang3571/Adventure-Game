d1c38330e6643a1275649cbe96766fe6aceb5843
Linfinity <wlin15@citymail.cuny.edu>
2015-11-18 00:55:48 -0500

    add some functions to character model

--- adventure_game/coreapp/models.py
+++ adventure_game/coreapp/models.py
@@ -1,5 +1,4 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.db impo
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class UserProfile(models.Model):
@@ -10,6 +9,16 @@ class UserProfile(models.Model):
 class Character(models.Model):
     user = models.ForeignKey(User)
     character_name = models.CharField(max_length=200)
-    character_pin = models.IntegerField(max_length=4, null=True)
+    character_pin = models.IntegerField(null=True)
+
     def __str__(self):
 		return self.character_name
+
+    def add_character(self, character, user=user):
+        user.character_name.add(character)
+
+    def add_pin(self, pin, user=user, character_name=character_name):
+        user.character_pin.add(pin)
+
+    def remove_character(self, character, user=user):
+        user.character_name.remove(character)
