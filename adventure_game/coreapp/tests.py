74e7349c542ea11cb1875428ad0a6c08bc342edf
Meghann B Bledsoe <mblesdo00@citymail.cuny.edu>
2015-11-18 01:49:56 -0500

    fixed test naming issue

--- adventure_game/coreapp/tests.py
+++ adventure_game/coreapp/tests.py
@@ -2,16 +2,20 @@ import unittest
 from django.test import TestCase
 from django.test import Client
 
-class AccountTests(unittest.TestCase):
+class AccountTests(TestCase):
 
     def setUp(self):
         self.user = Client()
     #registration test
-    def regi_test(self):
+    def test_regi(self):
         info = self.user.post('/registration-submission/',{'username': 'sam123', 'email': 'abc@gmail.com', 'password': 'abc123'})
         self.assertEqual(info.status_code,200)
 
     #login test
-    def login_test(self):
+    def test_login(self):
         response = self.user.post('/auth/',{'username': 'sam123', 'password': 'abc123'})
         self.assertEqual(response.status_code,200)
+
+class SimpleTest(TestCase):
+    def test_add(self):
+        self.assertEqual(1+1,2)
