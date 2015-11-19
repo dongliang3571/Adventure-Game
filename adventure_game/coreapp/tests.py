ae9ae1e8d623f24f0d77ce9ffe06696f09a56242
Meghann B Bledsoe <mblesdo00@citymail.cuny.edu>
2015-11-19 03:39:04 -0500

    separate 2 tests to get separate response codes

--- adventure_game/coreapp/tests.py
+++ adventure_game/coreapp/tests.py
@@ -9,7 +9,7 @@ from coreapp.views import registration_submission
 class AccountTests(TestCase):
 
     #login test with unregistred user
-    def test_login_redirect(self):
+    def test_login_fail(self):
         c=Client()
 
         #Supposed to fail. User is not registered - should give redirect code 302
@@ -17,15 +17,14 @@ class AccountTests(TestCase):
         self.assertRedirects(response,'invalid_login')
 
     #register user then login
-    def test_regi_and_login(self):
+    def test_regi(self):
         c2=Client()
 
         regi_response = c2.post('registrationSubmission',{'username':'janice22','firstname':'janice','lastname':'goralnik', 'email':'friends@show.com','password':'yemen'})
         self.assertEqual(regi_response.status_code,200)
 
-        login_response = c2.post('/auth/',{'username':'janice22','password':'yemen'})
-        self.assertEqual(login_response.status_code,200)
+    def test_login_pass(self):
 
-class SimpleTest(TestCase):
-    def test_add(self):
-        self.assertEqual(1+1,2)
+        c3=Client()
+        login_response = c3.post('/auth/',{'username':'janice22','password':'yemen'})
+        self.assertEqual(login_response.status_code,200)
