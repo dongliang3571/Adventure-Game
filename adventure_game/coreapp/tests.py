e07987ecb68e636e923bec2e63d7f16b216427df
Meghann B Bledsoe <mblesdo00@citymail.cuny.edu>
2015-11-19 03:33:22 -0500

    testing register test and invalid login redirect

--- adventure_game/coreapp/tests.py
+++ adventure_game/coreapp/tests.py
@@ -4,14 +4,27 @@ from django.test import Client
 from django.contrib.auth.models import User
 
 from coreapp.views import auth_view
+from coreapp.views import registration_submission
 
 class AccountTests(TestCase):
 
-    #login test
-    def test_login(self):
+    #login test with unregistred user
+    def test_login_redirect(self):
         c=Client()
+
+        #Supposed to fail. User is not registered - should give redirect code 302
         response = c.post('/auth/',{'username': 'sam123', 'password': 'abc123'})
-        self.assertEqual(response.status_code,200)
+        self.assertRedirects(response,'invalid_login')
+
+    #register user then login
+    def test_regi_and_login(self):
+        c2=Client()
+
+        regi_response = c2.post('registrationSubmission',{'username':'janice22','firstname':'janice','lastname':'goralnik', 'email':'friends@show.com','password':'yemen'})
+        self.assertEqual(regi_response.status_code,200)
+
+        login_response = c2.post('/auth/',{'username':'janice22','password':'yemen'})
+        self.assertEqual(login_response.status_code,200)
 
 class SimpleTest(TestCase):
     def test_add(self):
