3b308ed0acaf47c9b986028b15a98fe886831228
Meghann B Bledsoe <mblesdo00@citymail.cuny.edu>
2015-11-19 03:11:35 -0500

    fixed login test

--- adventure_game/coreapp/tests.py
+++ adventure_game/coreapp/tests.py
@@ -7,12 +7,10 @@ from coreapp.views import auth_view
 
 class AccountTests(TestCase):
 
-    def setUp(self):
-        self.c = Client()
-        self.c = User.objects.create_user(username="sam123", email="test@test.com", password="abc123")
     #login test
     def test_login(self):
-        response = self.c.login('/auth/',{'username': 'sam123', 'password': 'abc123'})
+        c=Client()
+        response = c.post('/auth/',{'username': 'sam123', 'password': 'abc123'})
         self.assertEqual(response.status_code,200)
 
 class SimpleTest(TestCase):
