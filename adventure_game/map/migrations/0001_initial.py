aca341ab130d033f96dec690648141b55568caca
Dong Liang <dliang04@citymail.cuny.edu>
2015-11-27 01:33:50 -0500

    input initial data

--- adventure_game/map/migrations/0001_initial.py
+++ adventure_game/map/migrations/0001_initial.py
@@ -2,20 +2,31 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-
-from django.core.management import call_command
-
-
-def loadfixture(apps, schema_editor):
-    call_command('loaddata', 'initial_data.json')
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('map', '0001_initial'),
+        ('auth', '0006_require_contenttypes_0002'),
     ]
 
     operations = [
-        migrations.RunPython(loadfixture),
+        migrations.CreateModel(
+            name='Level',
+            fields=[
+                ('user', models.OneToOneField(primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('level_number', models.IntegerField(default=0)),
+                ('question_number', models.IntegerField(default=1)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='QuestionAndAnswer',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('Question', models.CharField(max_length=200)),
+                ('Answer', models.CharField(max_length=200)),
+                ('QuestionNumber', models.IntegerField(default=1)),
+            ],
+        ),
     ]
