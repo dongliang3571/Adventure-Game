1fa92a96ccd21069bd4545089efb344b0b8a7a3d
Dong Liang <dliang04@citymail.cuny.edu>
2015-11-28 21:20:16 -0500

    corrected indentation

--- adventure_game/map/migrations/0002_auto_20151129_0108.py
+++ adventure_game/map/migrations/0002_auto_20151129_0108.py
@@ -10,16 +10,36 @@ def combine_names(apps, schema_editor):
     hint.objects.create(hint_text="888 Madision ave, New York, NY 10021")
     h=hint.objects.get(pk=1)
     questions = apps.get_model("map", "QuestionAndAnswer")
-    questions.objects.create(hint=h, Question="What is 3 + 5 = ?",Answer="8",QuestionNumber=1)
-    questions.objects.create(hint=h, Question="What is 4 + 4 = ?",Answer="8",QuestionNumber=2)
-    questions.objects.create(hint=h, Question="What is 7 + 1 = ?",Answer="8",QuestionNumber=3)
-    questions.objects.create(hint=h, Question="What is the 13th letter of the English alphabet?",Answer="m",QuestionNumber=4)
-    questions.objects.create(hint=h, Question="What is the 1st letter of the English alphabet?",Answer="a",QuestionNumber=5)
-    questions.objects.create(hint=h, Question="What is the 4th letter of the English alphabet?",Answer="d",QuestionNumber=6)
-    questions.objects.create(hint=h, Question="What is the 9th letter of the English alphabet?",Answer="i",QuestionNumber=7)
-    questions.objects.create(hint=h, Question="What is the 19th letter of the English alphabet?",Answer="s",QuestionNumber=8)
-    questions.objects.create(hint=h, Question="What is the 15th letter of the English alphabet?",Answer="o",QuestionNumber=9)
-    questions.objects.create(hint=h, Question="What is the 14th letter of the English alphabet?",Answer="n",QuestionNumber=10)
+    questions.objects.create(
+        hint=h, Question="What is 3 + 5 = ?", Answer="8", QuestionNumber=1
+    )
+    questions.objects.create(
+        hint=h, Question="What is 4 + 4 = ?", Answer="8", QuestionNumber=2
+    )
+    questions.objects.create(
+        hint=h, Question="What is 7 + 1 = ?", Answer="8", QuestionNumber=3
+    )
+    questions.objects.create(
+        hint=h, Question="What is the 13th letter of the English alphabet?", Answer="m",
+        QuestionNumber=4)
+    questions.objects.create(
+        hint=h, Question="What is the 1st letter of the English alphabet?", Answer="a",
+        QuestionNumber=5)
+    questions.objects.create(
+        hint=h, Question="What is the 4th letter of the English alphabet?", Answer="d",
+        QuestionNumber=6)
+    questions.objects.create(
+        hint=h, Question="What is the 9th letter of the English alphabet?", Answer="i",
+        QuestionNumber=7)
+    questions.objects.create(
+        hint=h, Question="What is the 19th letter of the English alphabet?", Answer="s",
+        QuestionNumber=8)
+    questions.objects.create(
+        hint=h, Question="What is the 15th letter of the English alphabet?", Answer="o",
+        QuestionNumber=9)
+    questions.objects.create(
+        hint=h, Question="What is the 14th letter of the English alphabet?", Answer="n",
+        QuestionNumber=10)
 
 
 class Migration(migrations.Migration):
--- adventure_game/map/views.py
+++ adventure_game/map/views.py
@@ -9,107 +9,110 @@ from django.contrib import messages
 def index(request):
     user=request.user
     if user.is_authenticated():
-        if user.is_superuser==True:
+        if user.is_superuser == True:
             messages.warning(request, 'Please login as a regular user to enter the map rather than a super user')
             return HttpResponseRedirect(reverse('coreapp:home'))
         else:
 
             ln=user.level.level_number
 
-            if ln==0:
-                boyn="boy"
-            elif ln==1:
-                boyn="boy boy1"
-            elif ln==2:
-                boyn="boy boy1 boy2"
-            elif ln==3:
-                boyn="boy boy1 boy2 boy3"
-            elif ln==4:
-                boyn="boy boy1 boy2 boy3 boy4"
+            if ln == 0:
+                boyn = "boy"
+            elif ln == 1:
+                boyn = "boy boy1"
+            elif ln == 2:
+                boyn = "boy boy1 boy2"
+            elif ln == 3:
+                boyn = "boy boy1 boy2 boy3"
+            elif ln == 4:
+                boyn = "boy boy1 boy2 boy3 boy4"
             messages.warning(request, 'Welcome to your adventures')
-            return render(request, 'map/index.html',{'boyn':boyn})
+            return render(request, 'map/index.html', {'boyn':boyn})
     else:
         messages.warning(request, 'Please sign in')
         return HttpResponseRedirect(reverse('coreapp:home'))
 
 def task1(request):
-    return render(request,'map/task1.html')
+    return render(request, 'map/task1.html')
 
 def task1_question1(request):
     if request.user.is_authenticated():
-        if request.user.level.task1_question1_completion==True:
+        if request.user.level.task1_question1_completion == True:
             return HttpResponseRedirect(reverse('map:task1_question2'))
-    # if request.user.level.question_number==11:
-    #     housenumber='888 Madision ave, New York, NY10021'
-    #     return render(request, 'map/task1_question1.html', {'message':'Congradulations, You have gotten your clue.','isComplete':'complete','houseNumber':housenumber})
-        ques = request.GET.get('question','')
-        ans = request.GET.get('answer','')
-        questionnumber = request.GET.get('questionNumber','')
+    # if request.user.level.question_number == 11:
+    #     housenumber = '888 Madision ave, New York, NY10021'
+    #     return render(request, 'map/task1_question1.html', {'message':'Congradulations, You have gotten your clue.', 'isComplete':'complete', 'houseNumber':housenumber})
+        ques = request.GET.get('question', '')
+        ans = request.GET.get('answer', '')
+        questionnumber = request.GET.get('questionNumber', '')
         if ans:
             correct_answer = QuestionAndAnswer.objects.get(Question=ques).Answer
-            if ans==correct_answer:
-                if request.user.level.question_number==10:
-                    # housenumber=''
+            if ans == correct_answer:
+                if request.user.level.question_number == 10:
+                    # housenumber = ''
                     # for n in range(request.user.level.question_number):
-                    #     questionTempObject=QuestionAndAnswer.objects.get(QuestionNumber=n+1)
-                    #     housenumber=housenumber+' '+str(questionTempObject.Answer)
+                    #     questionTempObject = QuestionAndAnswer.objects.get(QuestionNumber = n+1)
+                    #     housenumber = housenumber+' '+str(questionTempObject.Answer)
 
-                    housenumber='888 Madision ave, New York, NY 10021'
-                    user=request.user
-                    l = request.user.level
-                    l.task1_question1_completion = True;
-                    l.save()
-                    return render(request, 'map/task1_question1.html',{'isShow':'show','houseNumber':housenumber,
-                    'message':'Congradulations, You have gotten your clue.','isComplete':'complete'})
-                user=request.user
-                l = request.user.level
-                # l.level_number=l.level_number+1
-                l.question_number=l.question_number+1
-                l.save()
-                questionNumber=l.question_number
-                questionObject=QuestionAndAnswer.objects.get(QuestionNumber=questionNumber)
-                question=questionObject.Question
+                    hi = QuestionAndAnswer.objects.get(QuestionNumber=10).hint
+                    housenumber = hi.hint_text
+                    user = request.user
+                    le = request.user.level
+                    le.task1_question1_completion = True;
+                    le.save()
+                    return render(request, 'map/task1_question1.html', {'isShow':'show', 'houseNumber':housenumber,
+                    'message':'Congradulations, You have gotten your clue.', 'isComplete':'complete'})
+                user = request.user
+                le = request.user.level
+                # l.level_number = l.level_number+1
+                le.question_number = le.question_number+1
+                le.save()
+                questionNumber = le.question_number
+                questionObject = QuestionAndAnswer.objects.get(QuestionNumber=questionNumber)
+                question = questionObject.Question
 
-                housenumber=''
+                housenumber = ''
                 for n in range(questionNumber-1):
-                    questionTempObject=QuestionAndAnswer.objects.get(QuestionNumber=n+1)
-                    housenumber=housenumber+' '+str(questionTempObject.Answer)
+                    questionTempObject = QuestionAndAnswer.objects.get(QuestionNumber = n+1)
+                    housenumber = housenumber+' '+str(questionTempObject.Answer)
 
-                # if questionNumber==11:
-                #     # housenumber='888 Madision ave, New York, NY10021'
-                #     # return render(request, 'map/task1_question1.html', {'message':'Congradulations, You have gotten your clue.','isComplete':'complete','houseNumber':housenumber})
-                #     return render(request, 'map/task1_question1.html',{'message':'Congradulations, You have gotten your clue.','isComplete':'complete',
-                #     'isShow':'show','houseNumber':housenumber})
-                return render(request, 'map/task1_question1.html',{'message':'Your answer is correct, keep going.',
-                'isShow':'show','question':question,'houseNumber':housenumber})
+                # if questionNumber == 11:
+                #     # housenumber = '888 Madision ave, New York, NY10021'
+                #     # return render(request, 'map/task1_question1.html', {'message':'Congradulations, You have gotten your clue.', 'isComplete':'complete', 'houseNumber':housenumber})
+                #     return render(request, 'map/task1_question1.html', {'message':'Congradulations, You have gotten your clue.', 'isComplete':'complete',
+                #     'isShow':'show', 'houseNumber':housenumber})
+                return render(request, 'map/task1_question1.html', {'message':'Your answer is correct, keep going.',
+                'isShow':'show', 'question':question, 'houseNumber':housenumber})
             else:
-                user=request.user
-                questionNumber=user.level.question_number
-                questionObject=QuestionAndAnswer.objects.get(QuestionNumber=questionNumber)
-                question=questionObject.Question
-                housenumber=''
+                user = request.user
+                questionNumber = user.level.question_number
+                questionObject = QuestionAndAnswer.objects.get(QuestionNumber=questionNumber)
+                question = questionObject.Question
+                housenumber = ''
                 for n in range(questionNumber-1):
-                    questionTempObject=QuestionAndAnswer.objects.get(QuestionNumber=n+1)
-                    housenumber=housenumber+' '+str(questionTempObject.Answer)
-                return render(request, 'map/task1_question1.html',{'message2':'Sorry, Your answer is Wrong, Try again....',
-                'isShow':'show','question':question,'houseNumber':housenumber})
+                    questionTempObject = QuestionAndAnswer.objects.get(QuestionNumber=n+1)
+                    housenumber = housenumber+' '+str(questionTempObject.Answer)
+                return render(request, 'map/task1_question1.html', {'message2':'Sorry, Your answer is Wrong, Try again....',
+                'isShow':'show', 'question':question, 'houseNumber':housenumber})
         else:
-            user=request.user
-            questionNumber=user.level.question_number
-            questionObject=QuestionAndAnswer.objects.get(QuestionNumber=questionNumber)
-            question=questionObject.Question
-            housenumber=''
+            user = request.user
+            questionNumber = user.level.question_number
+            questionObject = QuestionAndAnswer.objects.get(QuestionNumber=questionNumber)
+            question = questionObject.Question
+            housenumber = ''
             for n in range(questionNumber-1):
-                questionTempObject=QuestionAndAnswer.objects.get(QuestionNumber=n+1)
-                housenumber=housenumber+' '+str(questionTempObject.Answer)
-            return render(request, 'map/task1_question1.html',{'isShow':'show','question':question,'houseNumber':housenumber})
+                questionTempObject = QuestionAndAnswer.objects.get(QuestionNumber = n+1)
+                housenumber = housenumber+' '+str(questionTempObject.Answer)
+            return render(request, 'map/task1_question1.html', {'isShow':'show', 'question':question, 'houseNumber':housenumber})
     else:
-        messages.warning(request,'Please sign in.')
+        messages.warning(request, 'Please sign in.')
         return HttpResponseRedirect(reverse('coreapp:home'))
 
 
 def task1_question2(request):
-    return render(request, 'map/task1_question2.html')
+    h = QuestionAndAnswer.objects.get(QuestionNumber = 10).hint
+    housenumber = h.hint_text
+    return render(request, 'map/task1_question2.html', {'houseNumber':housenumber})
 
 def task2(request):
-    return render(request,'map/task2.html')
+    return render(request, 'map/task2.html')
