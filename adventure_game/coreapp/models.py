2467d55da08ac8b70bfb814e231814d592d38af7
Linfinity <wlin15@citymail.cuny.edu>
2015-12-01 15:09:07 -0500

    added new added models to coreapp admin page

--- adventure_game/coreapp/models.py
+++ adventure_game/coreapp/models.py
@@ -16,8 +16,8 @@ class Character(models.Model):
         return self.character_name
 
 #############################################
-class Level(models.Model):
-    user = models.ForeignKey(User)
+class Level_num(models.Model):
+    user = models.OneToOneField(User)
     user_point = models.PositiveIntegerField(default=0)
     user_level_choices = (
         ('1', '1'),
@@ -34,16 +34,16 @@ class Level(models.Model):
 
 class Track(models.Model):
     user = models.ForeignKey(User)
-    adventures_done = models.CharField(blank=True, default='')
+    adventures_done = models.CharField(max_length=10, blank=True, default='')
 
     def __unicode__(self):
         return self.adventures_done
 
 
 class Game_saved(models.Model):
-    user = models.ForeignKey(User)
-    adventure_saved = models.CharField(blank=True, default='')  #adventure_id
-    task_saved = models.CharField(blank=True, default='')   #task_number
+    user = models.OneToOneField(User)
+    adventure_saved = models.CharField(max_length=10, blank=True, default='')  #adventure_id
+    task_saved = models.CharField(max_length=10, blank=True, default='')   #task_number
 
     def __unicode__(self):
         return 'Last saved at adventure id#'+str(self.adventure_saved) + 'task #:' + str(self.task_saved)
