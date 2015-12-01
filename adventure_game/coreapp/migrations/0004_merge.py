f5dc04e39311ade275ec3a94b5412caeaa63e6e9
Linfinity <wlin15@citymail.cuny.edu>
2015-11-30 22:16:26 -0500

    fix migrations bug

---
+++ adventure_game/coreapp/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('coreapp', '0003_character_is_logged'),
+        ('coreapp', '0003_merge'),
+    ]
+
+    operations = [
+    ]
