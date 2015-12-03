814f13e1c110f7e238a77f32c90df201ab4bf82c
Dong Liang <dliang04@citymail.cuny.edu>
2015-12-02 20:07:21 -0500

    added own context_processor

--- adventure_game/adventure_game/settings.py
+++ adventure_game/adventure_game/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'adventure_game.own_context_processors.islogged',
             ],
         },
     },
