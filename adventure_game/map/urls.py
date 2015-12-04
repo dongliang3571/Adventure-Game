feeb884c8a9c5b3f708ad07eec5416d87383d194
Linfinity <wlin15@citymail.cuny.edu>
2015-12-03 19:01:46 -0500

    fix tesksub url bug

--- adventure_game/map/urls.py
+++ adventure_game/map/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     url(r'^$', views.map, name='map'),
     url(r'^beginingstory$', views.beginingstory, name="beginingstory"),
     url(r'^task\d*$', views.task, name="task"),
-    url(r'^task2/$', views.task2, name="task2"),
+    # url(r'^task2/$', views.task2, name="task2"),
+    url(r'^tasksubmission', views.Task_Submission, name="tasksubmission")
     # url(r'^task1/task1_question1/$', views.task1_question1, name="task1_question1"),
     # url(r'^task1/task1_question2/$', views.task1_question2, name="task1_question2"),
 ]
