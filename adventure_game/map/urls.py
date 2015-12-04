32c4a7a74a4a722faa3a8ec395590eda3f9d61ca
Linfinity <wlin15@citymail.cuny.edu>
2015-12-04 03:33:17 -0500

    fix bug linking from map to task page & getting correct data

--- adventure_game/map/urls.py
+++ adventure_game/map/urls.py
@@ -6,9 +6,9 @@ from . import views
 urlpatterns = [
     url(r'^map$', views.map, name='map'),
     url(r'^story$', views.story, name="story"),
-    url(r'^task\d*$', views.task, name="task"),
+    # url(r'^task\d*$', views.task, name="task"),
+    url(r'^map/task\d$', views.task, name="task"),
     # url(r'^task2/$', views.task2, name="task2"),
-    url(r'^tasksubmission', views.Task_Submission, name="tasksubmission")
-    # url(r'^task1/task1_question1/$', views.task1_question1, name="task1_question1"),
-    # url(r'^task1/task1_question2/$', views.task1_question2, name="task1_question2"),
+    url(r'^tasksubmission', views.Task_Submission, name="tasksubmission"),
+
 ]
