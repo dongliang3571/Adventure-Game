10e97dc1f6eb87a2e39a8129f05526f46bc1ab55
Meghann B Bledsoe <mblesdo00@citymail.cuny.edu>
2015-12-07 22:15:19 -0500

    Integrating personas into tests to check the page from a users perspective.

---
+++ functional_tests.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_adventure(self):
+        #Daniel is a single parent who has heard about this
+        #new game that he can play with his daughter. So he
+        #picks a day and they open the site together.
+        self.browser.get('https://adventuregame.herokuapp.com')
+
+        #Daniel and his daughter see that the page title
+        #mentions adventures.
+        assert 'Adventure' in browser.title, "Browser title was " + browser.title
