833db398dfd792018dc3c7b3ab2aa27522f89ce4
Meghann B Bledsoe <mblesdo00@citymail.cuny.edu>
2015-12-10 09:05:12 -0500

    creating tests for tasks and map

--- adventure_game/map/tests.py
+++ adventure_game/map/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TaskTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='pass')
+        self.client.login(username='testuser', password='pass')
+
+class MapTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='pass')
+        self.client.login(username='testuser', password='pass')
