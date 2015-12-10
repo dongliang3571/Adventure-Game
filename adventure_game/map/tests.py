dfc2723707497e2cafe60d4de8b471c99502c23b
Meghann B Bledsoe <mblesdo00@citymail.cuny.edu>
2015-12-10 09:14:11 -0500

    test pre-adventure information and added user story information

--- adventure_game/map/tests.py
+++ adventure_game/map/tests.py
@@ -4,10 +4,21 @@ from django.contrib.auth.models import User
 # Create your tests here.
 class TaskTests(TestCase):
 
+    #Daniel and his daughter login to the account they created last time they
+    #went on an adventure.
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', password='pass')
         self.client.login(username='testuser', password='pass')
+
+    #They have selected the adventure to help the wizard and start adventure
+    #They now see a page showing them information about the adventure. The
+    #estimated time to complete, estimated cost, and any items needed to
+    #complete
+    def test_preadventure_information(self):
+        response = self.client.get('/information/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'map/information.html')
 
 class MapTests(TestCase):
 
@@ -15,3 +26,5 @@ class MapTests(TestCase):
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', password='pass')
         self.client.login(username='testuser', password='pass')
+
+    def test_(self):
