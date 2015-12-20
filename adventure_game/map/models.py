0dfa87aa3ca71e50072aae0aa7bf2c6e51d25b06
Dong Liang <dliang04@citymail.cuny.edu>
2015-12-20 03:07:22 -0500

    removed useless imgs, use url instead

--- adventure_game/map/models.py
+++ adventure_game/map/models.py
@@ -43,7 +43,7 @@ class Task(models.Model):
     task_type = models.CharField(max_length=10, choices = task_type_choices, default='Mission')
     task_description = models.TextField(max_length=2000, blank=True, default='')
     task_detail = models.TextField(max_length=2000, blank=True, default='')
-    place_img_url = models.URLField(blank=True, default='')
+    place_img_url = models.URLField(max_length=3000, blank=True, default='')
     task_ans = models.CharField(max_length=200, blank=True, default='')
     def __unicode__(self):
         return self.task_number
