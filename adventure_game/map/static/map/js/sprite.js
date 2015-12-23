88c8eafb608306775723e139f73029f493e5da27
Dong Liang <dliang04@citymail.cuny.edu>
2015-12-23 04:29:12 -0500

    removed useless code and imgs

--- adventure_game/map/static/map/js/sprite.js
+++ adventure_game/map/static/map/js/sprite.js
@@ -13,32 +13,10 @@ var count=0;
 // var female = new Image();
 var male = new Image();
 var tree = new Image();
-// var grand_terminal = new Image();
-// var empire_building = new Image();
-// var metropolitan_museum = new Image();
-// var statue_liberty = new Image();
-// var central_park = new Image();
-// var number1 = new Image();
-// var number2 = new Image();
-// var number3 = new Image();
-// var number4 = new Image();
-// var number5 = new Image();
 
-// female.src = "../static/map/images/characters/girl2.png";
-male.src="../static/map/images/characters/boy_sprite.png";
+male.src="http://i.imgur.com/BoJpt3z.png";
 tree.src="http://i444.photobucket.com/albums/qq168/grandmadeb_rmvx/Shareable%20Artists/BenBen%20CONFIRMED/BenBen%20Trees%20Seasonal_zps6pwibcy7.png";
 
-// grand_terminal.src="../static/map/images/buildings/grand.png";
-// empire_building.src="../static/map/images/buildings/Empire_State_Building.png";
-// metropolitan_museum.src="../static/map/images/buildings/metropolitan.png";
-// statue_liberty.src="../static/map/images/buildings/statue_liberty.png";
-// central_park.src="../static/map/images/buildings/central.png";
-//
-// number1.src="../static/map/images/numbers/number1.png"
-// number2.src="../static/map/images/numbers/number2.png"
-// number3.src="../static/map/images/numbers/number3.png"
-// number4.src="../static/map/images/numbers/number4.png"
-// number5.src="../static/map/images/numbers/number5.png"
 
 var ticksPerFrame = 10;
 var frameIndex_girl = 0;
@@ -80,34 +58,9 @@ function sprite(options) {
      that.context.drawImage(that.image,0,0,x,y,0,0,j,k);
    };
 
-  //  that.update = function () {
-  //   //  canvas.width = window.innerWidth;
-  //   //  canvas.height = window.innerHeight;
-   //
-  //       tickCount += 1;
-   //
-  //       if (tickCount > ticksPerFrame) {
-  //         tickCount = 0;
-  //         frameIndex += 1;
-  //         if (frameIndex >= numberOfFrames) {
-  //           frameIndex = 0;
-  //         }
-  //       }
-  //     };
-
-
     return that;
 }
 
-
-
-// var female_sprite = sprite({
-//     context: canvas_girl.getContext("2d"),
-//     width: 32,
-//     height: 48,
-//     image: female,
-//     frameIndex: frameIndex_girl
-// });
 
 var male_sprite = sprite({
     context: canvas_boy.getContext("2d"),
@@ -124,83 +77,7 @@ var tree_sprite = sprite({
     image: tree,
     frameIndex:0
 });
-//
-// var empire_building_sprite = sprite({
-//     context: canvas.getContext("2d"),
-//     width: 32,
-//     height: 48,
-//     image: empire_building
-// });
-//
-// var metropolitan_museum_sprite = sprite({
-//     context: canvas.getContext("2d"),
-//     width: 32,
-//     height: 48,
-//     image: metropolitan_museum
-// });
-//
-// var statue_liberty_sprite = sprite({
-//     context: canvas.getContext("2d"),
-//     width: 32,
-//     height: 48,
-//     image: statue_liberty
-// });
-//
-// var central_park_sprite = sprite({
-//     context: canvas.getContext("2d"),
-//     width: 32,
-//     height: 48,
-//     image: central_park
-// });
-//
-// var number1_sprite = sprite({
-//     context: canvas.getContext("2d"),
-//     width: 256,
-//     height: 256,
-//     image: number1
-// });
-//
-// var number2_sprite = sprite({
-//     context: canvas.getContext("2d"),
-//     width: 256,
-//     height: 256,
-//     image: number2
-// });
-//
-// var number3_sprite = sprite({
-//     context: canvas.getContext("2d"),
-//     width: 256,
-//     height: 256,
-//     image: number3
-// });
-//
-// var number4_sprite = sprite({
-//     context: canvas.getContext("2d"),
-//     width: 256,
-//     height: 256,
-//     image: number4
-// });
-//
-// var number5_sprite = sprite({
-//     context: canvas.getContext("2d"),
-//     width: 256,
-//     height: 256,
-//     image: number5
-// });
 
-// function draw(){
-//   number1_sprite.render_number(300,500);
-//   number2_sprite.render_number(400,450);
-//   number3_sprite.render_number(400,450);
-//   number4_sprite.render_number(400,450);
-//   number5_sprite.render_number(400,450);
-// }
-
-// function update(){
-//   female_sprite.update();
-//   // male_sprite.update();
-//
-// }
 function render_tree(){
   tree_sprite.render_static(200,310,100,150);
 }
@@ -211,12 +88,7 @@ function render_boy(){
     male_sprite.frameIndex=frameIndex_boy;
   }
 }
-// setInterval(render_girl, 250);
-// $(function(){
-//   $('#next').on('click',function(){
-//     $('#path4').append(" <b>Appended text</b>.");
-//   })
-// });
+
 $(document).on('keypress', function(e) {
     if(e.KeyCode == 13) {
         alert('You pressed enter!');
@@ -224,62 +96,10 @@ $(document).on('keypress', function(e) {
 });
 
 
-// $(document).ready(function(){
-//     $("#next").click(function(){
-//       count=count+1;
-//       if (count>4) {
-//         count=4;
-//       }
-//       if (count==1) {
-//         $("#boy").attr('class','boy boy1');
-//       }
-//       else if(count==2){
-//         $("#boy").attr('class','boy boy1 boy2');
-//       }
-//       else if(count==3){
-//         $("#boy").attr('class','boy boy1 boy2 boy3');
-//       }
-//       else if(count==4){
-//         $("#boy").attr('class','boy boy1 boy2 boy3 boy4');
-//       }
-//
-//
-//     });
-//
-//     $("#previous").click(function(){
-//       count=count-1;
-//       if (count<0) {
-//         count=0;
-//       }
-//       if (count==3) {
-//         $("#boy").attr('class','boy boy1 boy2 boy3');
-//       }
-//       else if(count==2){
-//         $("#boy").attr('class','boy boy1 boy2');
-//       }
-//       else if (count==1) {
-//         $("#boy").attr('class','boy boy1');
-//       }
-//       else if (count==0) {
-//         $("#boy").attr('class','boy');
-//       }
-//     });
-// });
+
 
 $(function() {
   setInterval(render_boy,250);
   setInterval(render_tree,10);
 
 });
-// function gameLoop () {
-  // update();
-  // female.addEventListener("load",render());
-  // draw();
-
-  // window.requestAnimationFrame(gameLoop);
-// }
-
-
-
-
-// number1.addEventListener("load", gameLoop);
