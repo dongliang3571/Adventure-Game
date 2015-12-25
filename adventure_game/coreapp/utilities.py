591a85607a65e520aad9f0db6c6e66d20c4205c5
Jun Hao (Raymond) Zhu <junhao.raymond.zhu@gmail.com>
2015-12-25 17:22:45 +0000

    Docstring for utilities

--- adventure_game/coreapp/utilities.py
+++ adventure_game/coreapp/utilities.py
@@ -1,8 +1,29 @@
+"""This module contains a bunch of utility functions used by the views.
+This is to minimize the amount of code inside views.
+"""
 from .queries import (load_adventures, load_completed_adventures,
                       load_game_save_id, get_character_name
                      )
 
 def get_profile_context(user, characters):
+    """This function makes queries to the database to get information about
+    the adventures and returns a dictionary with the information.
+
+    Parameters
+    ----------
+    user: User Object
+        The current logged in user.
+
+    characters: List
+        A list of Character models.
+
+    Returns
+    -------
+    context: Dictionary
+        Returns a dictionary container character name, level number,
+        game save data, adventure information, experience, and
+        rangelist.
+    """
     adventure_name_list = []
     adventure_img_url_list = []
     adventure_id_list = []
@@ -31,15 +52,25 @@ def get_profile_context(user, characters):
     return context
 
 def get_adventure_info(): #for home page, displays adv info
-        adventure_name_list = []
-        adventure_img_url_list = []
-        adventure_id_list = []
-        adventure_description_list = []
+    """This is a utility function that returns a dictionary containing level information
 
-        load_adventures(adventure_name_list, adventure_img_url_list, adventure_id_list, adventure_description_list)
-        zipped = zip(adventure_img_url_list, adventure_name_list, adventure_id_list, adventure_description_list)
+    Returns
+    -------
+        context: Dictionary
+            A dictionary with the key 'zipped' who's value is a list which contains
+            adventure name, list of image urls, ID, and description of the adventure.
+    """
+    adventure_name_list = []
+    adventure_img_url_list = []
+    adventure_id_list = []
+    adventure_description_list = []
 
-        context = {
-            'zipped' : zipped,
-        }
-        return context
+    load_adventures(adventure_name_list, adventure_img_url_list,
+                    adventure_id_list, adventure_description_list)
+    zipped = zip(adventure_img_url_list, adventure_name_list,
+                 adventure_id_list, adventure_description_list)
+
+    context = {
+        'zipped' : zipped,
+    }
+    return context
